6e095fbf16aa10302dc609c4dc2a03830b55dc29
CedricLeizour <cedricleizour@gmail.com>
2021-10-22 15:08:24 +0200

    footer + modal + responsive

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,8 +4,8 @@ import { Switch, Route } from 'react-router-dom';
 import Header from '../Header';
 import Footer from '../Footer';
 import Game from '../Game';
-import Modal from '../Modal';
 import Play from '../Play';
+
 
 // == Import
 import './styles.scss';
@@ -15,21 +15,21 @@ const App = () => {
   const [myChoice, setMyChoice] = useState('');
   const [score, setScore] = useState(0);
 
-return (
-  <div className="app">
-    <div className="container">
-      <Header score={score} />
-      <Switch>
-        <Route exact path="/">
-          <Play setMyChoice={setMyChoice} />
-        </Route>
-        <Route path="/game">
-          <Game myChoice={myChoice} score={score} setScore={setScore} />
-        </Route>
-      </Switch>
+  return (
+    <div className="app">
+      <div className="container">
+        <Header score={score} />
+        <Switch>
+          <Route exact path="/">
+            <Play setMyChoice={setMyChoice} />
+          </Route>
+          <Route path="/game">
+            <Game myChoice={myChoice} score={score} setScore={setScore} />
+          </Route>
+        </Switch>
+      </div>
+      <Footer />
     </div>
-    <Footer />
-  </div>
   );
 };
 
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'src/components/Footer/Modal';
 
 import './styles.scss';
 
-const Footer = () => (
-  <div>
-    Footer
-  </div>
-);
+const Footer = () => {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => {
+    setModal(!modal);
+  };
+
+  return (
+    <>
+      <footer className="footer">
+        <div className="attribution">
+          <p>Challenge by<a href="https://www.frontendmentor.io?ref=challenge" target="_blank"> Frontend Mentor</a>.</p>
+          <p> Coded by <a href="https://cedricleizour.netlify.app" target="_blank">Cédric Leizour</a>.</p>
+        </div>
+        <button className="rules" type="button" onClick={toggle}>
+          Rules
+        </button>
+      </footer>
+      {modal ? <Modal toggle={toggle} /> : null}
+    </>
+  );
+};
 
 export default Footer;
